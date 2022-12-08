9c6341ade8d41be42653b8e8842cd56e2477aabf
Abdulsatttar20 <a.sattar.balushi.20@gmail.com>
2022-12-07 18:33:21 -0600

    comments

--- controll.py
+++ controll.py
@@ -4,14 +4,21 @@ from forex_python.converter import CurrencyRates
 
 forex = CurrencyRates()
 class Forex(QMainWindow, Ui_MainWindow):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
+        '''
+        constructor to create initial state for foreign exchanger currency
+        '''
         super().__init__(*args, **kwargs)
         self.setupUi(self)
         self.exchange_Button.clicked.connect(lambda: self.exchange())
         self.clear_Button.clicked.connect(lambda: self.clear())
 
 
-    def exchange(self):
+    def exchange(self) -> None:
+        '''
+        this function teake the from currency and change it to the to currency
+        and take the amount that want to be exchanged
+        '''
         try:
 
             currency_from = self.from_comboBox.currentText()
@@ -30,6 +37,9 @@ class Forex(QMainWindow, Ui_MainWindow):
 
 
 
-    def clear(self):
+    def clear(self) -> None:
+        '''
+        this function clear all the text from the amount and the amount exchanged
+        '''
         self.input_amount.setText('')
         self.output_amount.setText('')
